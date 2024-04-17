f28f29f4fb95c9979e5ae00d3abe3105c019f79b
Prisha Shreevastav <112982045+PrishaS2007@users.noreply.github.com>
2024-04-17 11:21:13 -0600

    Update game-library.js

--- game-library.js
+++ game-library.js
@@ -70,11 +70,11 @@ function constrain(val, low, high) {
  * return: true or false
  */
 function ptInRect(x, y, rect) {
- if (((rect.x < x) && (x < (rect.x + rect.w))) && ((rect.y < y) && (y < (rect.y + rect.h)))) {
-  return true;
- } else {
-  return false;
- }
+  if (rect.x < x && x < rect.x + rect.w && rect.y < y && y < rect.y + rect.h) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /*
@@ -86,8 +86,8 @@ function ptInRect(x, y, rect) {
  * return: true or false
  */
 function ptInCircle(x, y, circle) {
-  var distPoint =  dist(x, y, circle.x, circle.y);
-  if (distPoint <  circle.r) {
+  let distPoint = dist(x, y, circle.x, circle.y);
+  if (distPoint < circle.r) {
     return true;
   } else {
     return false;
@@ -102,7 +102,12 @@ function ptInCircle(x, y, circle) {
  * return true or false
  */
 function rectCollide(rect1, rect2) {
-  if ((rect1.x < (rect2.x + rect2.w)) && ((rect1.x + rect1.w) > rect2.x) && ((rect1.y + rect1.h) > rect2.y) && (rect1.y < (rect2.y + rect2.h))) {
+  if (
+    rect1.x < rect2.x + rect2.w &&
+    rect1.x + rect1.w > rect2.x &&
+    rect1.y + rect1.h > rect2.y &&
+    rect1.y < rect2.y + rect2.h
+  ) {
     return true;
   } else {
     return false;
@@ -117,8 +122,8 @@ function rectCollide(rect1, rect2) {
  * return true or false
  */
 function circleCollide(circle1, circle2) {
-  var distPoint = dist(circle1.x, circle1.y, circle2.x, circle2.y)
-  if (distPoint < (circle1.r + circle2.r)) {
+  var distPoint = dist(circle1.x, circle1.y, circle2.x, circle2.y);
+  if (distPoint < circle1.r + circle2.r) {
     return true;
   } else {
     return false;
